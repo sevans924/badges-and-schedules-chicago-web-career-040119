84583a1693023ceabd8d3d2fd86a87b2403bb380
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 16:45:40 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -9,5 +9,5 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
-  array.each_with_index {|value, index| return "Hello, #{value}! You'll be assigned to room #{index + 1}!"}
+  array.each_with_index {|value, index| puts "Hello, #{value}! You'll be assigned to room #{index + 1}!"}
 end
