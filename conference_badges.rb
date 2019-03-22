fd1a240f34a97b15843f3fce32ffc0bedbedd312
Sarah Elzinga <sevans924@gmail.com>
2019-03-22 16:07:55 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,3 +2,7 @@ def badge_maker (name)
   return "Hello, my name is #{name}." # Write your code here.
 end
 # Write your code here.
+
+def batch_badge_creator(array)
+  array.each {|name| return "Hello, my name is #{name}"}
+end
